5e95f4e7289ceb66d3d758813e2458e3c6ffbcca
Robert Chiu <chiu.robert96@gmail.com>
2021-08-03 18:49:16 +0300

    Added API controller

--- transactioncompare.web/src/main/java/transactioncompare/web/TransactionComparatorWeb.java
+++ transactioncompare.web/src/main/java/transactioncompare/web/TransactionComparatorWeb.java
@@ -3,9 +3,13 @@ package transactioncompare.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+import transactioncompare.api.controller.TransactionCompareAPIController;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"transactioncompare.web", "transactioncompare.core"})
+@Import({TransactionCompareAPIController.class})
 public class TransactionComparatorWeb {
 	
 	public static void main(String[] args) {
